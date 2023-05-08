780f8525d719b58ffc523722dd5e05523e1cbe64
Patryk Kisielewski <pkisielewski@patkis.pl>
2023-05-08 20:32:05 +0200

    Initial commit

---
+++ src/main/java/pl/patkis/zielonykod/ZielonykodApplication.java
@@ -0,0 +1,13 @@
+package pl.patkis.zielonykod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZielonykodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZielonykodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/patkis/zielonykod/ZielonykodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patkis.zielonykod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZielonykodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
