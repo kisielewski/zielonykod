ca1bbded8c6ab63f2281a5213a8ce56e87c2a085
Patryk Kisielewski <pkisielewski@patkis.pl>
2023-05-12 02:16:41 +0200

    Change run.sh script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
-./gradlew bootRun
+# ./gradlew bootRun
+java -jar build/libs/zielonykod-1.0.0.jar
